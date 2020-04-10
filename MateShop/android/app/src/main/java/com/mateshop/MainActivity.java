b492b1f022bbaa9ca8f805a14fc84469ba8a7988
Ferran Romero <ferran.romero.dev@gmail.com>
2020-04-10 15:00:56 +0200

    Starting mateShop

--- MateShop/android/app/src/main/java/com/mateshop/MainActivity.java
+++ MateShop/android/app/src/main/java/com/mateshop/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rntemplate;
+package com.mateshop;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RNTemplate";
+    return "MateShop";
   }
 }
--- MateShop/android/app/src/main/java/com/mateshop/MainApplication.java
+++ MateShop/android/app/src/main/java/com/mateshop/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rntemplate;
+package com.mateshop;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.rntemplate.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.mateshop.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
