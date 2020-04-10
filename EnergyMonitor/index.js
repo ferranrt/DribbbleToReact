3d04e80de7f46f903512150c3a90396d8c0971dc
Ferran Romero <ferran.romero.dev@gmail.com>
2020-04-10 11:06:33 +0200

    Energy monitor remove

--- EnergyMonitor/index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import { name as appName } from './app.json';
-import { App } from './src/Containers';
-
-AppRegistry.registerComponent(appName, () => App);
--- EnergyMonitor/src/Components/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import Block from './Block';
-import Typography from './Typography';
-import Button from './Button';
-import Divider from './Divider';
-
-export { Block, Typography, Button, Divider };
